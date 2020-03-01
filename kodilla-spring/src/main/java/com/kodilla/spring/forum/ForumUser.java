af44bb1d5bc7b79c1220e82fc104786f6b3d2306
unknown <lukaszschweda@gmail.com>
2020-03-01 17:55:23 +0100

    zad-14.1

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
-    private String username = "John Smith";
+    private String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
 
     public String getUsername() {
         return username;
